74d7d0b5129c7843a4cfe4e7dd243e8b83aea378
matata <matata@PCC-3000004252.pcc.com.tw>
2018-08-08 15:16:35 +0800

    send assessmenttest.java

---
+++ src/test/java/test/test01/AssessmentTest.java
@@ -0,0 +1,51 @@
+package test.test01;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AssessmentTest {
+	
+	private Assessment myAssessment;
+
+	@Before
+	public void setUp() throws Exception {
+		myAssessment = new Assessment();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetGrade() {
+		int input = 100;
+		String expectedOutput = "A";
+		
+		String actualOutput = myAssessment.getGrade(input);
+		Assert.assertEquals(expectedOutput, actualOutput);
+	}
+	
+	@Test
+	public void testGetGrade2() {
+		int input = 80;
+		String expectedOutput = "B";
+		
+		String actualOutput = myAssessment.getGrade(input);
+		Assert.assertEquals(expectedOutput, actualOutput);
+	}
+	
+	@Test
+	public void testGetGrade3() {
+		int input = 60;
+		String expectedOutput = "D";
+		
+		String actualOutput = myAssessment.getGrade(input);
+		Assert.assertEquals(expectedOutput, actualOutput);
+	}
+
+}
